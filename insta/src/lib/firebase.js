8042d59c6967079e8b6388bf467490a53303071f
Johhanes Nguyen <johhanesnguyen@Johhaness-MacBook-Pro.local>
2021-02-22 13:17:48 -0800

    Firebase setup

---
+++ insta/src/lib/firebase.js
@@ -0,0 +1,15 @@
+const config = {
+    apiKey: "AIzaSyC_EAkIbFx7O4IUUXt-yQnUhcQgjKELuBc",
+    authDomain: "insta-f3560.firebaseapp.com",
+    databaseURL: "https://insta-f3560-default-rtdb.firebaseio.com",
+    projectId: "insta-f3560",
+    storageBucket: "insta-f3560.appspot.com",
+    messagingSenderId: "822704703941",
+    appId: "1:822704703941:web:c1cafc149f79d40de463ac"
+  };
+
+  const firebase = window.firebase.initializeApp(config);
+  // FieldValue is useful to make modification to data
+  const {FieldValue} = window.firebase.firestore;
+
+  export {firebase, FieldValue};
