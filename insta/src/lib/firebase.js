415237fb88f56b2c22c17d1d580e33a474136d86
Johhanes Nguyen <johhanesnguyen@Johhaness-MacBook-Pro.local>
2021-02-23 10:47:19 -0800

    hydrating Firestone with DB

--- insta/src/lib/firebase.js
+++ insta/src/lib/firebase.js
@@ -1,3 +1,5 @@
+// read this firestore
+// import {seedDatabase} from '../seed';
 const config = {
     apiKey: "AIzaSyC_EAkIbFx7O4IUUXt-yQnUhcQgjKELuBc",
     authDomain: "insta-f3560.firebaseapp.com",
@@ -11,5 +13,7 @@ const config = {
   const firebase = window.firebase.initializeApp(config);
   // FieldValue is useful to make modification to data
   const {FieldValue} = window.firebase.firestore;
+
+//   seedDatabase(firebase);
 
   export {firebase, FieldValue};
