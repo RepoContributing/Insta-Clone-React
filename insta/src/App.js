07dcca8371b8b75e42bd6755e4b59230fdc39e44
Johhanes Nguyen <johhanesnguyen@Johhaness-MacBook-Pro.local>
2021-02-24 12:57:50 -0800

    commenting line

--- insta/src/App.js
+++ insta/src/App.js
@@ -1,11 +1,12 @@
 import React, {lazy, Suspense} from 'react';
+
 const Dashboard = lazy(()=> import ('./pages/dashboard'));
 const Login = lazy(()=> import ('./pages/login'));
 const SignUp = lazy(() => import ('./pages/signup'));
 const Profile = lazy(() => import ('./pages/profile'));
 const NotFound = lazy(() => import ('./pages/not-found'));
 
-
+// this is a line to export App
 export default function App() {
   return (<p>Hello</p>)
 }
