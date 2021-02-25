c7a4d074b3231a773d755c154cb2920b1aed92ed
johnhongannguyen <johhaneshongan@gmail.com>
2021-02-25 01:16:32 -0800

    fixed

--- insta/src/constants/routes.js
+++ insta/src/constants/routes.js
@@ -3,11 +3,4 @@ export const LOGIN = '/login';
 export const SIGN_UP = '/signup';
 export const PROFILE = '/p/:username';
 export const NOT_FOUND = '/not-found';
-// this line for testing
-// testing
-<<<<<<< HEAD
-// testing 2 
-// no testing
-=======
-// testing 2 
->>>>>>> 0640153acd22920937c06cd60cd6ada070495fe9
+
--- insta/src/lib/firebase.js
+++ insta/src/lib/firebase.js
@@ -1,14 +1,23 @@
 // read this firestore
 // import {seedDatabase} from '../seed';
+// const config = {
+//     apiKey: "AIzaSyC_EAkIbFx7O4IUUXt-yQnUhcQgjKELuBc",
+//     authDomain: "insta-f3560.firebaseapp.com",
+//     databaseURL: "https://insta-f3560-default-rtdb.firebaseio.com",
+//     projectId: "insta-f3560",
+//     storageBucket: "insta-f3560.appspot.com",
+//     messagingSenderId: "822704703941",
+//     appId: "1:822704703941:web:c1cafc149f79d40de463ac"
+//   };
 const config = {
-    apiKey: "AIzaSyC_EAkIbFx7O4IUUXt-yQnUhcQgjKELuBc",
-    authDomain: "insta-f3560.firebaseapp.com",
-    databaseURL: "https://insta-f3560-default-rtdb.firebaseio.com",
-    projectId: "insta-f3560",
-    storageBucket: "insta-f3560.appspot.com",
-    messagingSenderId: "822704703941",
-    appId: "1:822704703941:web:c1cafc149f79d40de463ac"
-  };
+  apiKey: 'AIzaSyBzbDVpXMNirX9Frhm465m0vUFTNQhHM1w',
+  authDomain: 'instagram-e8841.firebaseapp.com',
+  databaseURL: 'https://instagram-e8841.firebaseio.com',
+  projectId: 'instagram-e8841',
+  storageBucket: 'instagram-e8841.appspot.com',
+  messagingSenderId: '1052235904858',
+  appId: '1:1052235904858:web:eac84ef7c0e15590916563'
+};
 
   const firebase = window.firebase.initializeApp(config);
   // FieldValue is useful to make modification to data
