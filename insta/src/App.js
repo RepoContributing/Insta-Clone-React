64b15f2669de9e2b47f3f543ddb3b7dc23ec9e37
johnhongannguyen <johhaneshongan@gmail.com>
2021-02-25 01:47:58 -0800

    set up React Router

--- insta/src/App.js
+++ insta/src/App.js
@@ -1,4 +1,6 @@
 import React, { lazy, Suspense } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 
 const Dashboard = lazy(() => import ('./pages/dashboard'));
 const Login = lazy(() => import ('./pages/login'));
@@ -8,8 +10,16 @@ const NotFound = lazy(() => import ('./pages/not-found'));
 
 export default function App() {
     return (
+        <Router>
         <Suspense fallback={<p>Loading ...</p>}> 
-        <p>This is where our content will be</p></Suspense>
-       
+            <Switch>
+                <Route path={ROUTES.LOGIN} component={Login} />
+                <Route path={ROUTES.SIGN_UP} component={SignUp} />
+                <Route path={ROUTES.PROFILE} component={Profile} />
+                <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+                <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+            </Switch>
+        </Suspense>
+        </Router>
         )
 }
