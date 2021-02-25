ef66271d40bd83254912cef6bef334625b9b65c0
johnhongannguyen <johhaneshongan@gmail.com>
2021-02-25 09:33:10 -0800

    doing fixed login page

--- insta/src/pages/login.js
+++ insta/src/pages/login.js
@@ -1,44 +1,21 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
-// =====================
-// Login.js page
-// =====================
 
-// =====================
-// Structure
-// =====================
-// - div (parent)
-// 	- div (child)
-// 	- div (child)
-// 		- div (child of child)
-// 		- div (child of child)
+// # Challenge
 
+// Add a document title of 'Login - Instagram' 
 
-// A container div that holds children
-// 	- div
-// 		- image of src /images/iphone-with-profile.jpg & alt tag of "iPhone with Instagram app"
+// Hint: Think about what React hook you'd use to apply the title - make sure that the hook chosen only runs on first render
 
-// 	- div to wrap the following children
-// 		- div -> (another div to wrap the form (see below for further details of the form)
-// 		- div -> a paragraph with a React router link that allows to the user to navigate to 'Sign up' - use the ROUTES file to link to this particular page
-
-// A form for the user to login with a method of POST
-
-// An input box for the user to enter their email address with a placeholder value of Email Address
-
-// An input box for the user to enter their password with a placeholder value of Password
-
-// A button so that the user can submit the form
-
-// References:
-// 	- Tailwind container: https://tailwindcss.com/docs/container
-// 	- Tailwind flex: https://tailwindcss.com/docs/flex
 export default function Login() {
+    useEffect(()=> {
+        document.title = 'Login - Instagram';
+    }, [])
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
-                <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+            <img src="/images/iphone-with-profile.jpg" alt="Instagram" className="mt-2 w-6/12 mb-4" />
             </div>
             <div className="flex flex-col w-2/5">
             <div className="flex flex-col items-center bg-white p-4 border mb-4">
--- insta/src/pages/signup.js
+++ insta/src/pages/signup.js
@@ -3,5 +3,6 @@ import React from 'react';
 export default function SignUp() {
     return (
         <p>Hello from SignUp</p>
+    
     )
 }
