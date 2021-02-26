3afeeaa1637fd6ba34caac6bc300c8bbe8723053
johnhongannguyen <johhaneshongan@gmail.com>
2021-02-25 22:59:13 -0800

    creating 404 page

--- insta/src/pages/dashboard.js
+++ insta/src/pages/dashboard.js
@@ -3,15 +3,7 @@ import Header from '../components/header.js';
 import Timeline from '../components/timeline.js';
 import Sidebar from '../components/sidebar/index.js';
 
-// Pull in the Header component and render it on the page
-// render all 3 components
-// Tailwind CSS documentation for grid/ grid-cols-3/ gap-4
-// figure how to use justify between 
-// figure how to use margin auto 
 
-// max-w-screen-lg 
-
-// Render Timeline and Sidebar in 
 export default function Dashboard() {
     useEffect(() =>{
         document.title = 'Instagram';
--- insta/src/pages/not-found.js
+++ insta/src/pages/not-found.js
@@ -4,9 +4,8 @@ export default function NotFound() {
     useEffect(() => {
         document.title = '404 - Not Found';
     }, []);
-    
     return (
-        <div className="bg-gray">
+        <div className="bg-gray-200">
             <div className="mx-auto max-w-screen-lg">
                 <p className="text-center text-2xl">Not Found!</p>
             </div>
